072d84768c92d461f09a07ea34043bf8ac2ce019
alu0101346908 <alu0101346908@ull.edu.es>
2022-02-23 09:33:54 +0000

    Comentarios

--- P1/main.py
+++ P1/main.py
@@ -1,10 +1,17 @@
+#Autor: Mario Hernandez Garcia
+#Email: alu0101346908@ull.edu.es
+
+#Modulo de la libreria random para generar numeros aleatorios
 from random import randrange
 
+#Costantes de colores para la salida por pantalla
 RED = "\033[2;31;40m"
 CYAN = "\033[2;36;40m"
 GREEN = "\033[2;32;40m"
 CLEAR_COLOR = "\033[0;0m"
 
+#Funcion que recibe la string de entrada por consola y la transforma a una string binaria, en caso de que la funcion de 
+# conversion no cubra los 8bits que definen un valor en la tabla ascii rellenamos con ceros ya que estos se omiten en la conversion
 def str_to_ascii_to_bin(input_string):
     result = ""
     for i in range(len(input_string)):
@@ -17,14 +24,19 @@ def str_to_ascii_to_bin(input_string):
             result += bin_result
     return result
 
+#Funcion que realiza la operacion xor entre dos strings binarias, para ello se recorre la string binaria valor a valor, 
+# se transforma en entero, se realiza la operacion xor y este se vuelve a transformar en string que se añadira a una vacia que retornaremos
 def xor_strings(s,t):
     return "".join(str(int(a,2) ^ int(b,2)) for a,b in zip(s,t))
 
+#Funcion que recibe una string binaria y la pasa a ASCII, para ello se recorre la string binaria de 8 en 8 posiciones, 
+# se pasa a entero y se utiliza char sobre ese entero para realizar la conversion, esto se añade en cada iteracion a una string nueva
 def bin_to_ascii_to_str(input_string):
     return "".join([chr(int(input_string[i:i+8],2)) for i in range(0,len(input_string),8)])
 
+#Funcion generadora de llaves la cual recibe el mensaje en binario para sacar su longitud y asi poder hacer que la clave tenga la misma longitud. 
+# La funcion random utiliza la longitud del mensaje como valor tope a generar aleatoriamente. El resultado normalmente tiene menor longitud por lo que lo rellenamos con 0 a la izquierda
 def random_key(bin_message):
-    #print (pow(2,len(message)))
     result = bin(randrange(pow(2,len(bin_message))))[2:]
     if (len(result) < len(bin_message)):
         while len(result) < len(bin_message):
